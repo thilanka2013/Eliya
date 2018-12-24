d5ac35b222ae6a5b1ca1c523c327253dc2db6f38
thilanka2013 <thilanka2013@gmail.com>
2018-12-24 14:25:24 +0530

    ggg
    nnnnn

---
+++ SampleWebSite/Controllers/ProfileController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebSite.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            ViewBag.Name = User.Identity.Name;
+            return View();
+        }
+    }
+}
